2e81310b95044862c10e9ec0563608be2d2181e3
Tom_docker <Tom_docker@jerry.com>
2024-09-04 08:45:56 +0800

    feat: test AES encrypt decrypt

--- src/main/java/encrypt/aes/AESPaddingTest.java
+++ src/main/java/encrypt/aes/AESPaddingTest.java
@@ -3,20 +3,23 @@ package encrypt.aes;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Arrays;
 
 public class AESPaddingTest {
 
     public static void main(String[] args) {
-        String inputFile = "D:\\aaa\\ScreenShot\\Screenshot_20240131-083143.png";
-        String encryptedFile = "D:\\aaa\\ScreenShot\\Screenshot_20240131-083143-encryptedFile.jpg";
-        String decryptedFile = "D:\\aaa\\ScreenShot\\Screenshot_20240131-083143-decryptedFile.jpg";
-        String keyString = "yourKey123456789"; // 16字节的密钥
+        String folder = "D:\\aaa\\Encrypt";
+        String inputFile = folder + "\\icon.jpg";
+        String encryptedFile = folder + "\\icon-encryptedFile.jpg";
+        String decryptedFile = folder + "\\icon-decryptedFile.jpg";
+        String keyString = "yourKey123456789yourKey123456789"; // 32字节的密钥
+        System.out.println("keyStringArray=" + Arrays.toString(keyString.getBytes(StandardCharsets.UTF_8)));
 
         try {
             // 加密
-            encryptFile(inputFile, encryptedFile, keyString);
+//            encryptFile(inputFile, encryptedFile, keyString);
 
             // 解密
             decryptFile(encryptedFile, decryptedFile, keyString);
@@ -31,14 +34,15 @@ public class AESPaddingTest {
         FileInputStream ins = null;
         FileOutputStream outs = null;
         try {
-            // 生成随机的初始化向量
+            // 生成随机的初始化向量 长度16字节
             SecureRandom random = new SecureRandom();
             byte[] ivBytes = new byte[16];
             random.nextBytes(ivBytes);
+            System.out.println("encrypt ivBytes=" + Arrays.toString(ivBytes));
             IvParameterSpec iv = new IvParameterSpec(ivBytes);
 
             // 创建密钥对象
-            byte[] keyBytes = Arrays.copyOf(keyString.getBytes("UTF-8"), 16);
+            byte[] keyBytes = Arrays.copyOf(keyString.getBytes(StandardCharsets.UTF_8), 32);
             SecretKey key = new SecretKeySpec(keyBytes, "AES");
 
             // 初始化加密器
@@ -86,14 +90,15 @@ public class AESPaddingTest {
         FileOutputStream outs = null;
 
         try {
-            // 读取初始化向量
+            // 读取初始化向量 长度16字节
             ins = new FileInputStream(inputFile);
             byte[] ivBytes = new byte[16];
             ins.read(ivBytes);
+            System.out.println("decrypt ivBytes=" + Arrays.toString(ivBytes));
             IvParameterSpec iv = new IvParameterSpec(ivBytes);
 
             // 创建密钥对象
-            byte[] keyBytes = Arrays.copyOf(keyString.getBytes("UTF-8"), 16);
+            byte[] keyBytes = Arrays.copyOf(keyString.getBytes(StandardCharsets.UTF_8), 32);
             SecretKey key = new SecretKeySpec(keyBytes, "AES");
 
             // 初始化解密器
@@ -104,7 +109,7 @@ public class AESPaddingTest {
             outs = new FileOutputStream(outputFile);
 
             // 解密文件内容
-            byte[] inputBuffer = new byte[16];
+            byte[] inputBuffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = ins.read(inputBuffer)) != -1) {
                 byte[] outputBuffer = cipher.update(inputBuffer, 0, bytesRead);
