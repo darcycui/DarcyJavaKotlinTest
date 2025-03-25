1c3f367d1315495c75ef7478603070c26523beac
Tom_docker <Tom_docker@jerry.com>
2025-03-25 16:44:01 +0800

    feat: add field init by password  function in TestFieldProxy

--- src/main/kotlin/proxy/TestFieldProxy.kt
+++ src/main/kotlin/proxy/TestFieldProxy.kt
@@ -1,6 +1,7 @@
 package org.example.proxy
 
 import java.util.*
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 fun main() {
@@ -13,20 +14,55 @@ fun main() {
  */
 class TestFieldProxy {
     fun test() {
-        val person = Person("tom")
-        println("person.name=${person.name}") // TOM
+        val person = Person(1,"tom","password1")
+        println(person) // TOM
         person.name = "jerry"
-        println("person.name=${person.name}")// JERRY
+        person.password = "password2"
+        println(person)// JERRY
 
     }
 }
 
 class Person() {
-    constructor(name: String): this() {
+    constructor(id: Int, name: String, passwordInit: String) : this() {
         this.name = name
+        this.password = passwordInit
     }
-    // TODO name属性委托给 UpperCaseProxy
+
+    // TODO name属性委托给 UpperCaseProxy对象
     var name: String by UpperCaseProxy()
+
+    // id属性委托给 lazy函数
+    private val id: Int by lazy {
+        -1
+    }
+
+    // TODO password属性委托给 password函数
+    var password: String by password {
+        "password default"
+    }
+
+    override fun toString(): String {
+        return "Person(name='$name', id=$id, password='$password')"
+    }
+
+}
+
+/**
+ * 属性委托函数
+ */
+fun <T> password(initializer: () -> T): ReadWriteProperty<Any?, T> {
+    // 返回ReadWriteProperty的实现类
+    return object : ReadWriteProperty<Any?, T> {
+        private var storedValue: T? = null
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            return storedValue as T
+        }
+
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+            storedValue = value
+        }
+    }
 }
 
 /**
