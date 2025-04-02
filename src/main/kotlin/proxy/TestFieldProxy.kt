f778729128e4ff8ce38e81a9e5ff2f3242f3e745
Tom_docker <Tom_docker@jerry.com>
2025-04-02 11:05:40 +0800

    feat: add field proxy implemented by lazyMutable()

--- src/main/kotlin/proxy/TestFieldProxy.kt
+++ src/main/kotlin/proxy/TestFieldProxy.kt
@@ -18,6 +18,7 @@ class TestFieldProxy {
         println(person) // TOM
         person.name = "jerry"
         person.password = "password2"
+        person.field = "field new"
         println(person)// JERRY
 
     }
@@ -26,14 +27,14 @@ class TestFieldProxy {
 class Person() {
     constructor(id: Int, name: String, passwordInit: String) : this() {
         this.name = name
-        this.password = passwordInit
+//        this.password = passwordInit
     }
 
     // TODO name属性委托给 UpperCaseProxy对象
     var name: String by UpperCaseProxy()
 
     // id属性委托给 lazy函数
-    private val id: Int by lazy {
+    val id: Int by lazy {
         -1
     }
 
@@ -42,10 +43,13 @@ class Person() {
         "password default"
     }
 
-    override fun toString(): String {
-        return "Person(name='$name', id=$id, password='$password')"
+    var field :String by lazyMutable {
+        "default field"
     }
 
+    override fun toString(): String {
+        return "Person(name='$name', id=$id, password='$password', field='$field')"
+    }
 }
 
 /**
@@ -56,6 +60,31 @@ fun <T> password(initializer: () -> T): ReadWriteProperty<Any?, T> {
     return object : ReadWriteProperty<Any?, T> {
         private var storedValue: T? = null
         override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            // 这里调用 initializer 初始化
+            if (storedValue == null) {
+                storedValue = initializer.invoke()
+            }
+            return storedValue as T
+        }
+
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+            storedValue = value
+        }
+    }
+}
+
+// 创建自定义的未初始化标记对象
+private object UNINITIALIZED
+
+fun <T> lazyMutable(initializer: () -> T): ReadWriteProperty<Any?, T> {
+    return object : ReadWriteProperty<Any?, T> {
+        private var storedValue: Any? = UNINITIALIZED
+
+        @Suppress("UNCHECKED_CAST")
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            if (storedValue == UNINITIALIZED) {
+                storedValue = initializer()
+            }
             return storedValue as T
         }
 
